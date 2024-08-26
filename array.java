526ee1d68459c4d11a34b0c2fbda64f5a18dcd73
vaishnaviranpise <156096994+vaishnaviranpise@users.noreply.github.com>
2024-08-26 15:07:54 +0530

    Add files via upload

---
+++ array.java
@@ -0,0 +1,16 @@
+import java.lang.*;
+class array
+{
+public static void main(String args[])
+{
+int r=5;
+for(int i=1;i<=r;i++)
+{
+for(int st=1;st=1;st++)
+{
+System.out.print("*");
+}
+System.out.println();
+}
+}
+}
