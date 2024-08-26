40f9597391f1ef24872be0530ba3194df6c461a2
vaishnaviranpise <156096994+vaishnaviranpise@users.noreply.github.com>
2024-08-26 15:09:47 +0530

    Add files via upload

---
+++ positive.java
@@ -0,0 +1,19 @@
+import java.lang.*;
+class positive
+{
+public static void main(String args[])
+{
+int num=-5;
+if(num==0)
+{
+System.out.println("number is zero");
+}
+else if(num>0)
+{
+System.out.println("number is positive");
+else
+{
+System.out.println("number is negative");
+}
+}
+}
