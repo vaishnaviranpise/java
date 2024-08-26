37424d4457d609b17eb628d546013a6b151d504e
vaishnaviranpise <156096994+vaishnaviranpise@users.noreply.github.com>
2024-08-26 15:10:27 +0530

    Add files via upload

---
+++ Switch.java
@@ -0,0 +1,14 @@
+import java.lang.*;
+class Switch
+{
+public static void main(String args[])
+{
+char color='9';
+switch(color)
+{
+case'1':System.out println("Red");
+case'2':System.out println("Black");
+case'3':System.out println("Blue");
+default:System.out println(" ");
+}
+}
